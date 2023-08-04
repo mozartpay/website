df16ae67236d2e69f5a84c9ed625a94e73bbce46
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-08-04 13:43:58 +0200

    purchase updates

--- src/layouts/transaction/confirm.tsx
+++ src/layouts/transaction/confirm.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaCheckCircle } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios'; 
 
 const SuccessPage: React.FC = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const code = searchParams.get('code');
+
+  const [purchase, setPurchase] = useState<any>(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5001/api/airtm/fetch/${code}`)
+      .then((response) => {
+        setPurchase(response.data.purchase);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [code]);
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <FaCheckCircle style={{ fontSize: '100px', color: 'green' }} />
       <h1 style={{ fontSize: '32px', fontWeight: 'bold', marginTop: '20px' }}>Transaction Approved</h1>
       <p style={{ fontSize: '18px', marginTop: '10px' }}>Transaction via Airtm</p>
-      <p style={{ fontSize: '18px', marginTop: '10px' }}>Transaction ID :</p>
-      <p style={{ fontSize: '18px', marginTop: '10px' }}>Status :</p>
-      <p style={{ fontSize: '18px', marginTop: '10px' }}>Amount :</p>
-      
+      <p style={{ fontSize: '18px', marginTop: '10px' }}>Transaction ID: {purchase?.id}</p>
+      <p style={{ fontSize: '18px', marginTop: '10px' }}>Amount: {purchase?.amount}</p>
+      <p style={{ fontSize: '18px', marginTop: '10px' }}>Date: {purchase?.created_at}</p>
+      <p style={{ fontSize: '18px', marginTop: '10px' }}>Status: {purchase?.status}</p>
     </div>
   );
 };
